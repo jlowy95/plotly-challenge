7193fa7fd6c5d7b61dd5bf828d51cb5340c31a92
jlowy95 <jlowy95@gmail.com>
2020-04-18 09:53:22 -0700

    Basic Challenge

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,127 @@
+function optionChanged(id) {
+    var sample_id = id;
+    console.log("Sample_id " + sample_id);
+    updateAll(sample_id);
+}
+
+function id2index(id, data) {
+    return data.names.indexOf(id);
+}
+
+function slice10rev(data){
+    return data.slice(0,10).reverse();
+}
+
+function init(){
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+
+        //Populate selector options
+        var selector = d3.select("#selDataset");
+        data.names.forEach((id) => {
+            var option = selector.append("option");
+            option.property("value",id);
+            option.text(id);
+        });
+
+        // Grab initial value of selector and find the index of it in the data
+        var idx = id2index(selector.property("value"), data);
+
+        plotBar(idx, data);
+        metadata(idx, data);
+        plotBubble(idx, data);
+    });
+}
+
+function plotBar(idx, data){
+
+    // Store the desired sample
+    var sample = data.samples[idx];
+
+    // Grab plot data: top 10 (pre sorted) then sort desc (reverse)
+    var sample_values = slice10rev(sample.sample_values).map(val => val);
+    // console.log(sample_values);
+    var plot_labels = slice10rev(sample.otu_ids).map(id => "OTU " + id);
+    // console.log(plot_labels);
+    var plot_hovers = slice10rev(sample.otu_labels).map(label => label);
+    // console.log(plot_hovers);
+
+    var hzbar_trace = {
+        orientation: "h",
+        x: sample_values,
+        y: plot_labels,
+        text: plot_hovers,
+        type: "bar"
+    };
+
+    var hzbar_data = [hzbar_trace];
+
+    Plotly.newPlot("bar", hzbar_data);
+}
+
+function metadata(idx, data){
+
+    // Select metadata div
+    var meta_div = d3.select("#sample-metadata");
+
+    // Clear any previous data
+    meta_div.html('');
+
+    // Show new data
+    var sample_meta = data.metadata[idx];
+    Object.entries(sample_meta).forEach(([key, value]) => {
+        var p_div = meta_div.append("p");
+        p_div.text(key + ": " + value);
+    });
+}
+
+function plotBubble(idx, data){
+
+    // Store the desired sample
+    var sample = data.samples[idx];
+
+    // Grab plot data: top 10 (pre sorted) then sort desc (reverse)
+    var bubble_y_size = sample.sample_values.map(val => val);
+    // console.log(sample_values);
+    var bubble_x_color = sample.otu_ids.map(id => id);
+    // console.log(plot_labels);
+    var bubble_text = sample.otu_labels.map(label => label);
+    // console.log(plot_hovers);
+
+    var bubble_trace = {
+        x: bubble_x_color,
+        y: bubble_y_size,
+        text: bubble_text,
+        // type: "bubble",
+        mode: "markers",
+        marker: {
+            size: bubble_y_size,
+            color: bubble_x_color
+        }
+    };
+
+    var layout = {
+        xaxis: {
+            title: "OTU ID"
+        }
+    };
+
+    var bubble_data = [bubble_trace];
+
+    Plotly.newPlot("bubble", bubble_data, layout);
+}
+
+function updateAll(sample_id){
+    d3.json("samples.json").then((data) => {
+        // Convert id to index
+        var idx = id2index(sample_id, data);
+        // Call all 3 update functions
+        plotBar(idx, data);
+        plotBubble(idx, data);
+        metadata(idx, data);
+    });
+}
+
+init();
+
+
