83ac6bf5f5e973d3164998c712d4371b8fd2f118
jlowy95 <jlowy95@gmail.com>
2020-04-22 17:55:44 -0700

    Finished Basic

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,18 +18,25 @@ function init(){
 
         //Populate selector options
         var selector = d3.select("#selDataset");
-        data.names.forEach((id) => {
-            var option = selector.append("option");
-            option.property("value",id);
-            option.text(id);
-        });
+        
+        selector.selectAll("option")
+            .data(data.names)
+            .enter()
+            .append("option")
+            .property("value",function(d){
+                return d;
+            })
+            .text(function(d){
+                return d;
+            });
 
         // Grab initial value of selector and find the index of it in the data
-        var idx = id2index(selector.property("value"), data);
+        var idx = id2index(d3.select("#selDataset").property("value"), data);
 
         plotBar(idx, data);
         metadata(idx, data);
         plotBubble(idx, data);
+        plotGauge(idx, data);
     });
 }
 
@@ -61,18 +68,25 @@ function plotBar(idx, data){
 
 function metadata(idx, data){
 
-    // Select metadata div
-    var meta_div = d3.select("#sample-metadata");
+    var sample_meta = data.metadata[idx];
 
-    // Clear any previous data
-    meta_div.html('');
+    // Select metadata div/data
+    var meta_div = d3.select("#sample-metadata").selectAll("p")
+        .data(Object.keys(sample_meta));
 
     // Show new data
-    var sample_meta = data.metadata[idx];
-    Object.entries(sample_meta).forEach(([key, value]) => {
-        var p_div = meta_div.append("p");
-        p_div.text(key + ": " + value);
-    });
+    // Object.entries(sample_meta).forEach(([key, value]) => {
+    //     var p_div = meta_div.append("p");
+    //     p_div.text(key + ": " + value);
+    // });
+    console.log(sample_meta);
+    meta_div.enter()
+        .append("p")
+        .merge(meta_div)
+        .text(function (key){
+            return key + ": " + sample_meta[key];
+        });
+    meta_div.exit().remove();
 }
 
 function plotBubble(idx, data){
@@ -111,6 +125,35 @@ function plotBubble(idx, data){
     Plotly.newPlot("bubble", bubble_data, layout);
 }
 
+function plotGauge(idx, data){
+
+
+    var gauge = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          value: 4,
+          title: { text: "Scrubs per Week" },
+          type: "indicator",
+          mode: "gauge",
+        //   delta: { reference: 380 },
+          gauge: {
+            axis: { range: [null, 9] },
+            steps: [
+              { range: [0, 2], color: "red" },
+              { range: [2,4], color: "orange" },
+              { range: [4,6], color: "yellow" },
+              { range: [6,8], color: "yellowgreen" },
+              { range: [8,10], color: "green" }
+            ]
+          }
+        }
+      ];
+      
+    //   var layout = {};
+
+      Plotly.newPlot('gauge', gauge);
+}
+
 function updateAll(sample_id){
     d3.json("samples.json").then((data) => {
         // Convert id to index
@@ -119,6 +162,7 @@ function updateAll(sample_id){
         plotBar(idx, data);
         plotBubble(idx, data);
         metadata(idx, data);
+        plotGauge(idx, data);
     });
 }
 
